cac677377b6134066a5c1fb23d058ee682989240
Mukerrem Akkoyun <mukerrem.akkoyun@nmqdigital.com>
2019-01-02 11:29:02 +0300

    Creation of component

---
+++ my-app/src/NewsItem.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+class NewsItem exdents Component {
+    
+    render(){
+
+        return(
+            <div>
+                <h1>Hello React</h1>
+                <p>Eos animi quis qui pariatur aliquam distinctio iure id quaerat. Architecto ducimus sequi. Ut necessitatibus placeat eum delectus. Saepe veniam laudantium quis id et autem aperiam eius et.</p>
+            </div>
+        )
+    }
+}
+
+export default NewsItem;
+
+// import React from 'react';
+
+// class NesItem exdents React.Component {
+
+// }

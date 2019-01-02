1d820fa4680448cb84b6bd8e439dd66da22d9ab3
Mukerrem Akkoyun <mukerrem.akkoyun@nmqdigital.com>
2019-01-02 19:42:27 +0300

    Newform created

--- my-app/src/components/NewsForm.js
+++ my-app/src/components/NewsForm.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 
 class NewsForm extends Component {
+
+    addButton() {
+        console.log('hello');
+        this.props.addNews();
+    }
+
     render() {
         return (
             <div>
-                
+                <button onClick={this.addButton.bind(this)}>Add</button>
             </div>
         );
     }

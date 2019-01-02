f905bc50c745e80444bef5b8f5420c5bd170d3ae
Mukerrem Akkoyun <mukerrem.akkoyun@nmqdigital.com>
2019-01-02 12:38:27 +0300

    added className

--- my-app/src/components/NewsItem.js
+++ my-app/src/components/NewsItem.js
@@ -6,7 +6,7 @@ class NewsItem extends Component {
 
         return(
             <div>
-                <h1>Hello React</h1>
+                <h1 className="test">Hello React</h1>
                 <p>Eos animi quis qui pariatur aliquam distinctio iure id quaerat. Architecto ducimus sequi. Ut necessitatibus placeat eum delectus. Saepe veniam laudantium quis id et autem aperiam eius et.</p>
             </div>
         )

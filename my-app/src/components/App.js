81314b875c7d90506aab7ff1fcc68bacefb943e1
Mukerrem Akkoyun <mukerrem.akkoyun@nmqdigital.com>
2019-01-02 19:51:56 +0300

    mutable

--- my-app/src/components/App.js
+++ my-app/src/components/App.js
@@ -3,26 +3,39 @@ import logo from "../logo.svg";
 import "../App.css";
 import News from "./News";
 
-class App extends Component {
-  render() {
-    const news = [
-      {
-        id: 1,
-        title: "title 1",
-        description: "description 1"
-      },
-      {
-        id: 2,
-        title: "title 2",
-        description: "description 2"
-      },
-      {
-        id: 3,
-        title: "title 3",
-        description: "description 3"
-      }
-    ];
+const news = [
+  {
+    id: 1,
+    title: "title 1",
+    description: "description 1"
+  },
+  {
+    id: 2,
+    title: "title 2",
+    description: "description 2"
+  },
+  {
+    id: 3,
+    title: "title 3",
+    description: "description 3"
+  }
+];
 
+class App extends Component {
+ 
+  addNews(){
+    console.log("runned");
+    news.push({
+      id:4,
+      title:'title 4',
+      description:'desc 4'
+    });
+
+    console.log(news);
+  }
+
+  render() {
+    
     return (
       <div className="App">
         <header className="App-header">
@@ -30,7 +43,7 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
-          <News news={news}  name="1sss"/>
+          <News news={news}  name="content" addNews={ this.addNews } />
         </header>
       </div>
     );
